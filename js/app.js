296f46f64ba4d77ed0dfeba0266cc7c494a5ea50
Paulo Moreira <moreira.a.paulo@gmail.com>
2015-08-22 19:12:59 -0500

    Partial implementation of Enemy/Player classes and methods

--- js/app.js
+++ js/app.js
@@ -6,7 +6,14 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-}
+    this.x = 1;
+
+    //Setting initial position below water tile;
+    this.y = 172;
+
+    this.speed = 1;
+
+};
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -14,21 +21,60 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-}
+
+    this.x *= dt;
+    this.y *= dt;
+
+
+};
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+
+    //Subclassing Enemy class
+    Enemy.call(this);
+    
+
+    //Setting default character  
+    this.sprite = 'images/char-cat-girl.png';
+    
+};
+
+
+//Delegating methods to Enemy class
+Player.prototype = Object.create(Enemy.prototype);
+
+//Correctly setting constructor
+Player.prototype.constructor = Player;
+
+// Player.prototype.handleInput = handleInput(){
+
+
+
+
+
+// };
+
+
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+
+var enemy1 = new Enemy();
+var player = new Player();
+var allEnemies = [];
+
+allEnemies.push(enemy1);
+
 
 
 
